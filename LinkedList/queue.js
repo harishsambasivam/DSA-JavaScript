87e21df00f151d01f9bbad5c559e320e23cf0a29
harishsambasivam <imharishsambasivam@gmail.com>
2021-01-16 12:56:47 +0530

    Added Queue using linkedlist

--- LinkedList/queue.js
+++ LinkedList/queue.js
@@ -1,1 +1,75 @@
+/* Queue implementation using LinkedList
+ * A Queue is a data structure that allows you to add an element to the end of
+ * a list and remove the item at the front. A queue follows a "First In First Out"
+ * system, where the first item to enter the queue is the first to be removed. This
+ * implementation uses an array to store the queue.
+ */
+
+// Functions: enqueue, dequeue, peek, view
+
 // Queue implementation using LinkedList
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  enqueue(value) {
+    const node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      const oldTail = this.tail;
+      oldTail.next = node;
+      this.tail = node;
+    }
+    this.length++;
+    return node;
+  }
+
+  dequeue() {
+    if (this.length === 0) return undefined;
+    let oldHead = this.head;
+    if (this.length == 1) {
+      this.tail = null;
+    }
+    this.head = oldHead.next;
+    this.length--;
+    return oldHead.value;
+  }
+
+  peek() {
+    return this.head.value;
+  }
+
+  view() {
+    const queue = [];
+    let currentNode = this.head;
+    while (currentNode) {
+      queue.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return queue;
+  }
+}
+
+let queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
+queue.enqueue(5);
+
+for (let i = 0; i < 5; i++) {
+  console.log(`Served ${queue.dequeue()}`);
+  console.log(queue.view());
+}
--- LinkedList/stack.js
+++ LinkedList/stack.js
@@ -1,7 +1,4 @@
-/*
-
- ****Stack implementation using LinkedList****
-
+/* Stack implementation using LinkedList
 * A stack is exactly what it sounds like. An element gets added to the top of
 * the stack and only the element on the top may be removed. This is an example
 * of an array implementation of a Stack. So an element can only be added/removed
@@ -11,66 +8,64 @@
 
  */
 
-class Node{
-    constructor(value){
-        this.value = value;
-        this.next = null;
-    }
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
-class Stack{
-    constructor(){
-        this.head = null;
-        // The top of the Stack
-        this.top = 0;
-    }
+class Stack {
+  constructor() {
+    this.head = null;
+    // The top of the Stack
+    this.top = 0;
+  }
 
-    // Adds a value onto the beginning of the stack
-    push(value){
-        let node = new Node(value);
-        if(!this.head) {
-            this.head = node;
-        }
-        else {
-            let oldHead = this.head;
-            this.head = node;
-            this.head.next = oldHead;
-        }
-        return this.top++;
+  // Adds a value onto the beginning of the stack
+  push(value) {
+    let node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+    } else {
+      let oldHead = this.head;
+      this.head = node;
+      this.head.next = oldHead;
     }
+    return this.top++;
+  }
 
-    // Removes and returns the value at the beginning of the stack
-    pop(){
-        if(!this.head) return undefined;
-        let oldHead = this.head;
-        let newHead = oldHead.next;
-        this.head = newHead;
-        this.top--;
-        return oldHead.value;
-    }
+  // Removes and returns the value at the beginning of the stack
+  pop() {
+    if (!this.head) return undefined;
+    let oldHead = this.head;
+    let newHead = oldHead.next;
+    this.head = newHead;
+    this.top--;
+    return oldHead.value;
+  }
 
-    // To see all the elements in the stack
-    view(){
-        if(!this.head) return undefined;
-        let currentNode = this.head;
-        while(currentNode){
-            console.log(currentNode.value);
-            currentNode = currentNode.next;
-        }
+  // To see all the elements in the stack
+  view() {
+    if (!this.head) return undefined;
+    let currentNode = this.head;
+    while (currentNode) {
+      console.log(currentNode.value);
+      currentNode = currentNode.next;
     }
+  }
 
-    // Returns the value at the top of the stack
-    peek(){
-        if(!this.head) return undefined;
-        return this.head.value;
-    }
+  // Returns the value at the top of the stack
+  peek() {
+    if (!this.head) return undefined;
+    return this.head.value;
+  }
 
-    // Returns the size of the stack
-    length(){
-        return this.top;
-    }
+  // Returns the size of the stack
+  length() {
+    return this.top;
+  }
 }
-
 
 let stack = new Stack();
 stack.push(1);
