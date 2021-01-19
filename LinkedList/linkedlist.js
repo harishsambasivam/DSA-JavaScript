7f2d621f94acb10234bad46d7ca7c12c3b3d4d93
harishsambasivam <imharishsambasivam@gmail.com>
2021-01-19 07:21:33 +0530

    added get,set,insert,delete,print methods

--- LinkedList/linkedlist.js
+++ LinkedList/linkedlist.js
@@ -59,6 +59,7 @@ class LinkedList {
     return ++this.length;
   }
 
+  // to remove element from front of the linkedlist
   shift() {
     if (this.length === 0) return undefined;
     let currentHead = this.head;
@@ -71,6 +72,66 @@ class LinkedList {
     }
     this.length--;
     return currentHead.value;
+  }
+
+  // to get an element in particular index
+  get(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    let currentNode = this.head;
+    for (let i = 0; i < index; i++) {
+      currentNode = currentNode.next;
+    }
+    return currentNode;
+  }
+
+  // to update an element in particular index
+  set(index, value) {
+    let node = this.get(index);
+    if (node != undefined) {
+      node.value = value;
+      return true;
+    }
+    return false;
+  }
+
+  // insert an node into linkedlist
+  insert(index, value) {
+    if (index < 0 || index > this.length) return undefined;
+    if (index === 0) return this.unshift(value);
+    if (index === this.length) return this.push(value);
+    let node = new Node(value);
+    let previousNode = this.get(index - 1);
+    let nextNode = previousNode.next;
+    previousNode.next = node;
+    node.next = nextNode;
+    return ++this.length;
+  }
+
+  // to remove an node from the linkedlist
+
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length) return this.pop();
+    let previousNode = this.get(index - 1);
+    let node = previousNode.next;
+    let nextNode = node.next;
+    previousNode.next = nextNode;
+    // unlinking from linkedlist chain
+    node.next = null;
+    return node.value;
+  }
+
+  // to print the list
+
+  print() {
+    let list = [];
+    let currentNode = this.head;
+    while (currentNode) {
+      list.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return list;
   }
 }
 
