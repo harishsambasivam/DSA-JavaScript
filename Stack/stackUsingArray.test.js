5cf0a1420c8ae8a7c546bfb8a3c007f2de2b67c0
harishsambasivam <imharishsambasivam@gmail.com>
2021-01-20 06:56:39 +0530

    fixed unit tests

--- Stack/stackUsingArray.test.js
+++ Stack/stackUsingArray.test.js
@@ -1,33 +1,32 @@
-const { expect } = require('@jest/globals');
-const {Stack} = require('./stackUsingLinkedList');
+const { expect } = require("@jest/globals");
+const { Stack } = require("./stackUsingArray");
 const stack = new Stack();
 
-describe('Stack implementation using linked list', () => {
-    it('should push elements to stack!',() => {
-        expect(stack.push(1)).toBe(0);
-        expect(stack.push(2)).toBe(1);
-        expect(stack.push(3)).toBe(2);
-        expect(stack.push(4)).toBe(3);
-    })
-    
-    it('should print the stack!', () => {
-        expect(stack.view().toString()).toBe([1,2,3,4].toString());
-        const emptyStack = new Stack();
-        expect(emptyStack.view().toString()).toBe([].toString());
-    });
-    
-    it('should pop elements from the stack!', () => {
-        expect(stack.pop()).toBe(4);
-        expect(stack.pop()).toBe(3);
-        expect(stack.pop()).toBe(2);
-        expect(stack.pop()).toBe(1);
-        expect(stack.pop()).toBeUndefined();
-    });
-    
-    
-    it('should print the top element of stack!', () => {
-        expect(stack.push(1)).toBe(0);
-        expect(stack.push(2)).toBe(1);
-        expect(stack.peek()).toBe(2);
-    });
+describe("Stack implementation using linked list", () => {
+  it("should push elements to stack!", () => {
+    expect(stack.push(1)).toBe(0);
+    expect(stack.push(2)).toBe(1);
+    expect(stack.push(3)).toBe(2);
+    expect(stack.push(4)).toBe(3);
+  });
+
+  it("should print the stack!", () => {
+    expect(stack.view().toString()).toBe([1, 2, 3, 4].toString());
+    const emptyStack = new Stack();
+    expect(emptyStack.view().toString()).toBe([].toString());
+  });
+
+  it("should pop elements from the stack!", () => {
+    expect(stack.pop()).toBe(4);
+    expect(stack.pop()).toBe(3);
+    expect(stack.pop()).toBe(2);
+    expect(stack.pop()).toBe(1);
+    expect(stack.pop()).toBeUndefined();
+  });
+
+  it("should print the top element of stack!", () => {
+    expect(stack.push(1)).toBe(0);
+    expect(stack.push(2)).toBe(1);
+    expect(stack.peek()).toBe(2);
+  });
 });
--- Stack/stackUsingLinkedList.test.js
+++ Stack/stackUsingLinkedList.test.js
@@ -1,44 +1,41 @@
-const { expect} = require('@jest/globals');
-const {Stack} = require('./stackUsingArray');
+const { expect } = require("@jest/globals");
+const { Stack } = require("./stackUsingLinkedList");
 const stack = new Stack();
 
-describe('Stack implementation using JavaScript Array', () => {
-    it('should push elements to stack!',() => {
-        expect(stack.push(1)).toBe(0);
-        expect(stack.push(2)).toBe(1);
-        expect(stack.push(3)).toBe(2);
-        expect(stack.push(4)).toBe(3);
-    })
-    
-    it('should print the stack!', () => {
-        expect(stack.view().toString()).toBe([4,3,2,1].toString());
-        const emptyStack = new Stack();
-        expect(emptyStack.view().toString()).toBe([].toString());
-    });
-    
-    it('should pop elements from the stack!', () => {
-        expect(stack.pop()).toBe(4);
-        expect(stack.pop()).toBe(3);
-        expect(stack.pop()).toBe(2);
-        expect(stack.pop()).toBe(1);
-        expect(stack.pop()).toBeUndefined();
-    });
-    
-    
-    it('should print the top element of stack!', () => {
-        expect(stack.push(1)).toBe(0);
-        expect(stack.push(2)).toBe(1);
-        expect(stack.peek()).toBe(2);
-    });
-    
-    
-    
-    it('should print the length of the stack!', () => {
-        stack.pop();
-        stack.pop();
-        expect(stack.length()).toBe(0);
-        stack.push(1);
-        stack.push(2);
-        expect(stack.length()).toBe(2);
-    })
+describe("Stack implementation using JavaScript Array", () => {
+  it("should push elements to stack!", () => {
+    expect(stack.push(1)).toBe(0);
+    expect(stack.push(2)).toBe(1);
+    expect(stack.push(3)).toBe(2);
+    expect(stack.push(4)).toBe(3);
+  });
+
+  it("should print the stack!", () => {
+    expect(stack.view().toString()).toBe([4, 3, 2, 1].toString());
+    const emptyStack = new Stack();
+    expect(emptyStack.view().toString()).toBe([].toString());
+  });
+
+  it("should pop elements from the stack!", () => {
+    expect(stack.pop()).toBe(4);
+    expect(stack.pop()).toBe(3);
+    expect(stack.pop()).toBe(2);
+    expect(stack.pop()).toBe(1);
+    expect(stack.pop()).toBeUndefined();
+  });
+
+  it("should print the top element of stack!", () => {
+    expect(stack.push(1)).toBe(0);
+    expect(stack.push(2)).toBe(1);
+    expect(stack.peek()).toBe(2);
+  });
+
+  it("should print the length of the stack!", () => {
+    stack.pop();
+    stack.pop();
+    expect(stack.length()).toBe(0);
+    stack.push(1);
+    stack.push(2);
+    expect(stack.length()).toBe(2);
+  });
 });
